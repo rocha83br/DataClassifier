89bf0b02488e2b7cbb665eba3705ca19ca492875
Renato Rocha <rocha83br@gmail.com>
2021-06-17 03:58:05 -0300

    Port to .Net Core 3.1 and hotfixes

---
+++ Rochas.DataClassifier.TestConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Rochas.DataClassifier.TestConsole.Mock;
+
+namespace Rochas.DataClassifier.TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sourceData = JsonConvert.DeserializeObject<ICollection<Nature>>(
+                File.OpenText("Mock\\NatureData.json").ReadToEnd());
+
+            using (var classifier = new RochasClassifier())
+            {
+                foreach (var item in sourceData)
+                    classifier.AddGroup(item.Category);
+
+                foreach (var item in sourceData)
+                    classifier.Train(item.Category, string.Concat(item.Name, " ", item.Description));
+
+                classifier.PrepareSearchTree();
+
+                var result = classifier.Classify("magnoliaceae chinese");
+            }
+        }
+    }
+}
---
+++ Rochas.DataClassifier/Dependencies/PTStemmer/Helpers/Datastructures/SuffixTreeNode.cs
@@ -0,0 +1,89 @@
+/**
+ * PTStemmer - A Stemming toolkit for the Portuguese language (C) 2008-2010 Pedro Oliveira
+ * Emprovements to parallel objects on .NET 4.5 (C) 2017 Renato Rocha 
+ * 
+ * This file is part of PTStemmer.
+ * PTStemmer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * PTStemmer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with PTStemmer. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+using System;
+using System.Collections.Concurrent;
+
+namespace PTStemmer.Helpers.Datastructures
+{
+	/// <summary>
+	/// Object-oriented Suffix Tree node
+	/// </summary>
+	public class SuffixTreeNode<T>
+	{		
+		private readonly ConcurrentDictionary<char, SuffixTreeNode<T>> edges;
+		private T val;
+		private bool isNull;
+		
+		public SuffixTreeNode()
+		{
+			edges = new ConcurrentDictionary<char,SuffixTreeNode<T>>();
+		}
+		
+		public SuffixTreeNode(T val): this()
+		{
+			this.val = val;
+		}
+		
+		private SuffixTreeNode<T> addEdge(char c, T val, bool isNull)
+		{
+			SuffixTreeNode<T> node;
+			if(!edges.TryGetValue(c, out node))
+			{
+				node = new SuffixTreeNode<T>(val);
+				edges.TryAdd(c, node);
+				if(isNull)
+					node.isNull = true;
+			}
+			return node;
+		}
+		
+		public SuffixTreeNode<T> addEdge(char c, T val)
+		{
+			return addEdge(c,val,false);
+		}
+		
+		public SuffixTreeNode<T> addEdge(char c)
+		{
+			return addEdge(c,default (T), true);
+		}
+	
+		public SuffixTreeNode<T> this[char c]
+		{
+			get {
+				SuffixTreeNode<T> node;
+				edges.TryGetValue(c, out node);
+				return node;
+				}
+		}
+
+		public T Value
+		{
+			get { return this.val; }
+			set { this.val = value; }
+		}
+		
+		public bool ValueIsNull
+		{
+			get {return this.isNull;}
+			set {this.isNull = value; }
+		}
+	}
+}
